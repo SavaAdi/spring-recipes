ddcef9cf7b8d5c8ff307e26659b6c01d224fc196
SavaAdi <50299766+SavaAdi@users.noreply.github.com>
2019-09-29 16:54:40 +0300

    further testing indexController

--- src/test/java/com/adisava/springrecipes/controllers/IndexControllerTest.java
+++ src/test/java/com/adisava/springrecipes/controllers/IndexControllerTest.java
@@ -1,17 +1,20 @@
 package com.adisava.springrecipes.controllers;
 
+import com.adisava.springrecipes.domain.Recipe;
 import com.adisava.springrecipes.services.RecipeService;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.junit.Assert.*;
-import static org.mockito.ArgumentMatchers.anySet;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 public class IndexControllerTest {
 
@@ -30,9 +33,23 @@ public class IndexControllerTest {
 
     @Test
     public void getIndex() {
+//        given
+        Set<Recipe> recipes = new HashSet<>();
+        recipes.add(new Recipe());
+        
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+        recipes.add(recipe);
+
+        when(recipeService.getRecipes()).thenReturn(recipes);
+        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+//        when
         String view = indexController.getIndex(model);
+//        then
         assertEquals("index",view);
         verify(recipeService,times(1)).getRecipes();
-        verify(model,times(1)).addAttribute(eq("recipes"),anySet());
+        verify(model,times(1)).addAttribute(eq("recipes"),argumentCaptor.capture());
+        Set<Recipe> set = argumentCaptor.getValue();
+        assertEquals(2,set.size());
     }
 }
